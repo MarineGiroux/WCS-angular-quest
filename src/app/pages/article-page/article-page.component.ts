94b29608f6eed5953b5e8c6fb8654bfb0d8ea12a
MarineGiroux <85103261+MarineGiroux@users.noreply.github.com>
2024-11-13 08:23:32 +0100

    added quest-10

--- src/app/pages/article-page/article-page.component.ts
+++ src/app/pages/article-page/article-page.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Article } from '../../models/article.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-article-page',
@@ -9,8 +12,13 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
   styleUrl: './article-page.component.scss'
 })
 export class ArticlePageComponent {
-  private route: ActivatedRoute = injecting(ActivatedRoute);
+  private route: ActivatedRoute = inject(ActivatedRoute);
+  private http = inject(HttpClient);
   articleId!: number;
+
+  getArticleById(id:number): Observable<Article>{
+    return this.http.get<Article>('http://localhost:3000/articles/:id');
+  }
 
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
